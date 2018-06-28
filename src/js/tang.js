4521576cbbcc1370b81c97033d2f4a8ef2066caf
tangguotong <tangguotong@163.com>
2018-06-28 17:18:52 +0800

    页面

--- src/js/tang.js
+++ src/js/tang.js
@@ -4,4 +4,22 @@ $(function () {
         checkboxClass: 'icheckbox_square-blue',
         radioClass: 'iradio_square-blue '
     });
+
+    //时间插件初始化
+    $('.date-select').daterangepicker({
+        "locale": {
+            format: 'YYYY-MM-DD',
+            separator: ' ~ ',
+            applyLabel: "应用",
+            cancelLabel: "取消",
+            resetLabel: "重置",
+            customRangeLabel: '自定义',
+            daysOfWeek: ['日', '一', '二', '三', '四', '五', '六'],
+            monthNames: ['一月', '二月', '三月', '四月', '五月', '六月',
+                '七月', '八月', '九月', '十月', '十一月', '十二月'
+            ],
+        }
+    },function () {
+
+    });
 });
