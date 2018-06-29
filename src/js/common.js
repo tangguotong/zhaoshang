32c60e1f13dddb421b8d692a494bcdc01ad75f17
qingyun <1348426268@qq.com>
2018-06-29 15:40:06 +0800

    提交页面

---
+++ src/js/common.js
@@ -0,0 +1,15 @@
+/**
+ * createby to ayun 2018/6/29
+ */
+
+/* 公共js */
+
+/**
+ * 初始化选择框
+ */
+$(function(){
+    $('input[type=checkbox]').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        increaseArea: '20%'
+    });
+})
