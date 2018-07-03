d5acab4fa44040a70bb6d415bb11b79350700a68
tangguotong <tangguotong@163.com>
2018-07-03 15:28:04 +0800

    园区对比详情页+修改input框

--- src/js/gardencontrastdetail.js
+++ src/js/gardencontrastdetail.js
@@ -1,8 +1,10 @@
 $(function () {
     //第一个echart
-    createEchartOption(document.getElementById('echart1'))
-    createEchartOption(document.getElementById('echart2'))
-    createEchartOption(document.getElementById('echart3'))
+    createEchartOption(document.getElementById('echart1'));
+    createEchartOption(document.getElementById('echart2'));
+    createEchartOption(document.getElementById('echart3'));
+    initQuadrantCoordinateChart(document.getElementById('echart4'));
+
 
 });
 //绘制柱状图
@@ -117,4 +119,141 @@ function createEchartOption(dom,map) {
         }]
     };
     echart.setOption(option);
+}
+
+//生成散点图
+function initQuadrantCoordinateChart(dom, data, averageSatisfaction) {
+    data = [[8, 21, "中国（广东）自由贸易试验区广州南沙新区"],[6, 71, "江苏经济技术开发区"],[2, 60, "东营经济技术开发区"]];
+    var data2 = [[6, 10, "中国（广东）自由贸易试验区广州南沙新区"],[7, 71, "江苏经济技术开发区"],[5, 60, "东营经济技术开发区"]];
+    averageSatisfaction = 0.14;
+    var myChart = echarts.init(dom);
+
+    myChart.showLoading({
+        text: "正在努力的读取数据中..."
+    });
+    var option = {
+        grid: {
+            left: 20,
+            top: '10%',
+            right: 20,
+            bottom: '3%',
+            containLabel: true
+        },
+        tooltip: {
+            showDelay: 0,
+            formatter: function(params) {
+                if (params.value.length > 1) {
+                    return params.value[2] + ' <br/>值1: ' + params.value[0] + ' <br/>值2: ' + params.value[1];
+                } else {
+                    return params.name + ' : ' + params.value;
+                }
+            },
+            axisPointer: {
+                show: true,
+                type: 'cross',
+                lineStyle: {
+                    width: 1
+                }
+            }
+        },
+        xAxis: [{
+            nameGap: 25,
+            type: 'value',
+            scale: true,
+            axisLine: {
+                show: true,
+                lineStyle: {
+                    color: "#999999",
+                    width: 1,
+                    type: "solid"
+                },
+            },
+            axisTick: {
+                show: false,
+            },
+            axisLabel: {
+                show: true,
+            },
+            splitLine:{
+                show:false
+            }
+        }],
+        yAxis: [{
+            // nameLocation: 'middle',
+            type: 'value',
+            scale: true,
+            min: 0,
+            axisLine: {
+                show: true,
+                lineStyle: {
+                    color: "#999999",
+                    width: 1,
+                    type: "solid"
+                },
+            },
+            axisTick: {
+                show: false,
+            },
+            axisLabel: {
+                show: true,
+            },
+            splitLine:{
+                show:false
+            }
+        }],
+        series: [
+            {
+                    // name: '声量&满意度',
+                    type: 'scatter',
+                    data: data,
+                    symbolSize: 15,
+                    itemStyle: {
+                        normal: {
+                            /*  color: '#67d8a9',*/
+                            color:"#367ce4",
+                            label: {
+                                show: true,
+                                position: 'right',
+                                formatter: function(params) {
+                                    return params.value[2];
+                                }
+                            }
+                        }
+                    }
+            },
+
+            {
+                name: '声量&满意度',
+                type: 'scatter',
+                data: data2,
+                symbolSize: 15,
+                itemStyle: {
+                    normal: {
+                        /*  color: '#67d8a9',*/
+                        color:"#ff9c00",
+                        label: {
+                            show: true,
+                            position: 'right',
+                            formatter: function(params) {
+                                return params.value[2];
+                            }
+                        }
+                    }
+                }
+            },
+        ]
+    };
+    myChart.setOption(option);
+    myChart.hideLoading();
+    // return myChart;
+}
+
+//生成单个横向柱形图
+function createOneItemBar(color,value,max) {
+    var width = value/max *200;
+    var left = width + 15;
+    var html = '<div class="item-bar">' +
+        '<span class="rect" style="width:'+width+'px;background-color: '+color+'"></span>' +
+        '<span class="num" style="left:'+left+'px">'+value+'</span></div>';
+    return html;
 }
