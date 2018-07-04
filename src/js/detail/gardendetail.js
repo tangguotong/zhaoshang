adaab6bcf79c1cb303945063165874a85bed05d7
qingyun <1348426268@qq.com>
2018-07-04 10:43:30 +0800

    提交页面

--- src/js/detail/gardendetail.js
+++ src/js/detail/gardendetail.js
@@ -6,11 +6,14 @@
 /*园区评价*/
 
 
-$(function () {
-    $('#myTab a[href="#tab2"]').tab('show')
-})
+$(function(){
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+       if($(e.target).text() == "园区情报"){
+           showecharts()
+       }
 
-
+    });
+});
 
 var sourceData = [{
     name: '马山县',
@@ -60,137 +63,145 @@ var avg = {
     servicesAvgLine: computeServicesAvgLine(),
     salesAvgLine: computeSalesAvgLine()
 };
-var scatter=echarts.init(document.getElementById("scatter"));
+function showecharts(){
+    var scatter=echarts.init(document.getElementById("scatter"));
 
-var option = {
+    var option = {
 
-    grid: {
-        bottom: '40%',
-        left: '6%'
-    },
-    tooltip: {
-        trigger: 'item',
-        axisPointer: {
-            show: true,
-            type: 'cross',
-            lineStyle: {
-                type: 'dashed',
-                width: 1
+        grid: {
+            bottom: '10%',
+            top:"5%"
+        },
+        tooltip: {
+            trigger: 'item',
+            axisPointer: {
+                show: true,
+                type: 'cross',
+                lineStyle: {
+                    type: 'dashed',
+                    width: 1
+                },
             },
-        },
-        formatter: function(obj) {
-            if (obj.componentType == "series") {
-                return '<div style="border-bottom: 1px solid rgba(255,255,255,.3); font-size: 18px;padding-bottom: 7px;margin-bottom: 7px">' +
-                    obj.name +
-                    '</div>' +
-                    '<span>' +
-                    '新办率' +
-                    '</span>' +
-                    ' : ' + obj.data.value[1] + '%' +
-                    '<br/>' +
-                    '<span>' +
-                    '注销率' +
-                    '</span>' +
-                    ' : ' + obj.data.value[0] + '%'
-            }
-        }
-    },
-    label: {
-        normal: {
-            show: true,
-            position: 'bottom',
-            formatter: function(params) {
-                return params.name
+            formatter: function(obj) {
+                if (obj.componentType == "series") {
+                    return '<div style="border-bottom: 1px solid rgba(255,255,255,.3); font-size: 18px;padding-bottom: 7px;margin-bottom: 7px">' +
+                        obj.name +
+                        '</div>' +
+                        '<span>' +
+                        '新办率' +
+                        '</span>' +
+                        ' : ' + obj.data.value[1] + '%' +
+                        '<br/>' +
+                        '<span>' +
+                        '注销率' +
+                        '</span>' +
+                        ' : ' + obj.data.value[0] + '%'
+                }
             }
         },
-        emphasis: {
-            show: true,
-            position: 'bottom'
-        }
-    },
-    yAxis: {
-
-        min: 8,
-        type: 'value',
-        scale: true,
-        axisLabel: {
-            formatter: '{value} %'
-        },
-
-        splitLine: {
-            lineStyle: {
-                type: 'dashed'
-            }
-        },
-        axisLine: {
-            lineStyle: {
-                color: '#000'
-            }
-        }
-    },
-    xAxis: {
-
-        type: 'value',
-
-        splitLine: {
-            show: false
-        },
-        axisLine: {
-            lineStyle: {
-                color: '#000'
-            }
-        }
-    },
-
-    series: [{
-        type: 'scatter',
-        data: seriesData,
-        symbolSize: 10,
-        color: '#367ce4',
         label: {
             normal: {
                 show: true,
-                formatter: function(sourceData) {
-                    return sourceData.name;
-                },
-                position: 'right'
+                position: 'bottom',
+                formatter: function(params) {
+                    return params.name
+                }
+            },
+            emphasis: {
+                show: true,
+                position: 'bottom'
             }
         },
-        markLine: {
-            label: {
-                normal: {
+        yAxis: {
 
+            min: 8,
+            type: 'value',
+            scale: true,
+            axisLabel: {
+                formatter: '{value} %'
+            },
+
+            splitLine: {
+                lineStyle: {
+                    type: 'dashed'
                 }
             },
-            lineStyle: {
+            axisLine: {
+                lineStyle: {
+                    color: '#000'
+                }
+            }
+        },
+        xAxis: {
+
+            type: 'value',
+            name: '社会评价声量',
+            nameLocation: 'middle',
+            splitLine: {
+                show: false
+            },
+            axisLine: {
+                lineStyle: {
+                    color: '#000'
+                }
+            }
+        },
+
+        series: [{
+            type: 'scatter',
+            data: seriesData,
+            symbolSize: 10,
+            itemStyle: {
                 normal: {
                     color: "#367ce4",
-                    type: 'solid',
-                    width: 1
-                },
-                emphasis: {
-                    color: "#d9def7"
                 }
             },
-            data: [{
-                xAxis: avg.salesAvgLine,
-
-                itemStyle: {
-                    normal: {
-                        color: "#b84a58",
-                    }
+            label: {
+                normal: {
+                    show: true,
+                    color: '#367ce4',
+                    formatter: function(sourceData) {
+                        return sourceData.name;
+                    },
+                    position: 'right'
                 }
-            }, {
-                yAxis: avg.servicesAvgLine,
-
-                itemStyle: {
+            },
+            markLine: {
+                label: {
                     normal: {
-                        color: "#b84a58",
-                    }
-                }
-            }]
-        }
-    }]
-};
 
-scatter.setOption(option);
+                    }
+                },
+                lineStyle: {
+                    normal: {
+                        color: "#367ce4",
+                        type: 'solid',
+                        width: 1
+                    },
+                    emphasis: {
+                        color: "#d9def7"
+                    }
+                },
+                data: [{
+                    xAxis: avg.salesAvgLine,
+
+                    itemStyle: {
+                        normal: {
+                            color: "#b84a58",
+                        }
+                    }
+                }, {
+                    yAxis: avg.servicesAvgLine,
+
+                    itemStyle: {
+                        normal: {
+                            color: "#b84a58",
+                        }
+                    }
+                }]
+            }
+        }]
+    };
+    scatter.setOption(option);
+}
+
