6b7387a9a506cf4092df6cfde8d2ca883bd63ab9
tangguotong <tangguotong@163.com>
2018-07-05 15:27:31 +0800

    修改页面

--- src/js/tang.js
+++ src/js/tang.js
@@ -6,21 +6,40 @@ $(function () {
     });
 
     //时间插件初始化
-    $('.date-select').daterangepicker({
-        "locale": {
-            format: 'YYYY-MM-DD',
-            separator: ' ~ ',
-            applyLabel: "应用",
-            cancelLabel: "取消",
-            resetLabel: "重置",
-            customRangeLabel: '自定义',
-            daysOfWeek: ['日', '一', '二', '三', '四', '五', '六'],
-            monthNames: ['一月', '二月', '三月', '四月', '五月', '六月',
-                '七月', '八月', '九月', '十月', '十一月', '十二月'
-            ],
+   //
+    /**
+     * 初始化日期插件
+     */
+    $(".date-select").daterangepicker({
+        maxDate : moment(), //最大时间
+        dateLimit : {
+            days : 180
+        },//起止时间的最大间隔
+        opens: 'center',
+        format : 'YYYY-MM-DD', //控件中from和to 显示的日期格式
+        separator : ' ~ ',
+        autoUpdateInput: false,
+        buttonClasses: 'btn',
+        applyClass: 'btn-blue',
+        cancelClass: 'btn-default',
+        locale: {
+            applyLabel : '确定',
+            cancelLabel : '取消',
+            fromLabel : '起始时间',
+            toLabel : '结束时间',
+            customRangeLabel : '自定义',
+            daysOfWeek : [ '日', '一', '二', '三', '四', '五', '六' ],
+            monthNames : [ '一月', '二月', '三月', '四月', '五月', '六月',
+                '七月', '八月', '九月', '十月', '十一月', '十二月' ],
+            firstDay : 1
         }
-    },function () {
-
+    }).on('apply.daterangepicker', function(ev, picker) {
+        alert(123 )
+        $(this).val(picker.startDate.format('YYYY-MM-DD') + ' ~ ' + picker.endDate.format('YYYY-MM-DD'));
+        $("input[name=startDate]").val(picker.startDate.format('YYYY-MM-DD'));
+        $("input[name=endDate]").val(picker.endDate.format('YYYY-MM-DD'));
+    }).on('cancel.daterangepicker', function(ev, picker) {
+        $(this).val('');
     });
 
 
